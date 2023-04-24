0a34a32373257a9ce9b7c40abd1aef6095380144
xharlesd <bernidocharlesdavid@gmail.com>
2023-04-25 00:19:30 +0800

    read integers.txt line by line

--- squared even cubed odd numbers.py
+++ squared even cubed odd numbers.py
@@ -5,12 +5,14 @@
 
 # open integers.txt (read), double.txt(write), triple.txt(write)
 with open("integers.txt", 'r') as input_file, open("double.txt", 'w') as output_even,  open("triple.txt", 'w') as output_odd:
-    """"""
-# read integers.txt line by line
-# convert each line from integers.txt into integer
-# if the extracted number is even
-    # extracted number will be squared
-    # squared even numbers will be written to double.txt
-# else if the extracted number is odd
-    # extracted number will be tripled
-    # cubed odd numbers will be written to double.txt
+    
+    # read integers.txt line by line
+    for line in input_file:
+        """"""
+        # convert each line from integers.txt into integer
+        # if the extracted number is even
+            # extracted number will be squared
+            # squared even numbers will be written to double.txt
+        # else if the extracted number is odd
+            # extracted number will be tripled
+            # cubed odd numbers will be written to double.txt
